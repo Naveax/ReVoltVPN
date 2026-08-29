15fe8f0d8ed6f355bae4579dd89af17537d5ee78
Naveax <Omersevik095@gmail.com>
2026-08-29 20:17:45 +0300

    fix: make haptic feedback app-wide

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,20 @@ import 'dart:ui';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
+import 'package:revoltvpn/components/app_haptic_surface.dart';
 import 'package:revoltvpn/logic/vpn_connection.dart';
 import 'package:revoltvpn/logic/session_timer.dart';
 import 'package:revoltvpn/logic/ad_manager.dart';
 import 'package:revoltvpn/logic/server_list.dart';
 import 'package:revoltvpn/logic/app_colors.dart';
+import 'package:revoltvpn/logic/haptic_settings.dart';
 import 'package:revoltvpn/screens/intro.dart';
-void main() {
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await HapticSettings.initialize();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
@@ -57,6 +61,8 @@ class ReVoltApp extends StatelessWidget {
         title: 'ReVolt VPN',
         debugShowCheckedModeBanner: false,
         theme: _buildDarkTheme(),
+        builder: (context, child) =>
+            AppHapticSurface(child: child ?? const SizedBox.shrink()),
         home: const IntroScreen(),
       ),
     );
